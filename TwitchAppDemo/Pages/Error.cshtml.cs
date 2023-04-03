70965b77c829c3bb53096dd0c2013208979782fe
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-04-03 10:16:39 +0000

    Renamed app

--- TwitchAppDemo/Pages/Error.cshtml.cs
+++ TwitchAppDemo/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace App.Pages;
+namespace TwitchAppDemo.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
