8eb0b829e3a2a26cd0c21bfa3b5284b4df0f3d83
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-04-03 12:33:59 +0000

    adding github actions

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -2,3 +2,16 @@ variable "app_location" {
   type    = string
   default = "australiaeast"
 }
+
+variable "ACR_NAME" {
+  type = string
+  default = "DMCTwitchACR"
+}
+
+variable "DOCKER_REGISTRY_USERNAME" {
+  type = string
+}
+
+variable "DOCKER_REGISTRY_PASSWORD" {
+  type= string
+}
