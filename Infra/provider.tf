87b8cfc5ae33af784f8ba2a53d6be4ee85e9fd92
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-04-03 11:07:19 +0000

    Adding basic iac

---
+++ Infra/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "3.50.0"
+        }
+    }
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstate27067"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {}
+}
